f7dc1876051b37e8f9955256620a297ab2a199c3
Myeongseop Jeon <jeonms9338@snu.ac.kr>
2026-02-13 16:14:29 +0900

    Do not use relative import in omas_imas.py

--- vaft/imas/from_omas/omas_imas.py
+++ vaft/imas/from_omas/omas_imas.py
@@ -31,9 +31,9 @@
 -------
 '''
 
-from .omas_utils import *
-from .omas_core import ODS, codeparams_xml_save, codeparams_xml_load, dynamic_ODS, omas_environment
-from .omas_utils import _extra_structures
+from omas.omas_utils import *
+from omas.omas_core import ODS, codeparams_xml_save, codeparams_xml_load, dynamic_ODS, omas_environment
+from omas.omas_utils import _extra_structures
 
 
 class IDS:
